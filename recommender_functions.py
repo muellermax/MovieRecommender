3d9a0a4ea0bfb2b719c56d5ce0cfc0b107753d74
dermaxihalt <muellermax1985@gmail.com>
2020-07-12 00:35:41 +0200

    started working on fit Method of recommender
    Added Read_csv of data
    Preparation of data, using unstack and converting to array
    Result: user-item matrix

--- recommender_functions.py
+++ recommender_functions.py
@@ -1,15 +1,16 @@
 import numpy as np
 import pandas as pd
 
+
 def get_movie_names(movie_ids, movies_df):
-    '''
+    """
     INPUT
     movie_ids - a list of movie_ids
     movies_df - original movies dataframe
     OUTPUT
     movies - a list of movie names associated with the movie_ids
+    """
 
-    '''
     # Read in the datasets
     movie_lst = list(movies_df[movies_df['movie_id'].isin(movie_ids)]['movie'])
 
@@ -17,48 +18,49 @@ def get_movie_names(movie_ids, movies_df):
 
 
 def create_ranked_df(movies, reviews):
-        '''
-        INPUT
-        movies - the movies dataframe
-        reviews - the reviews dataframe
+    """
+    INPUT
+    movies - the movies dataframe
+    reviews - the reviews dataframe
 
-        OUTPUT
-        ranked_movies - a dataframe with movies that are sorted by highest avg rating, more reviews, then time, and must have more than 4 ratings
-        '''
+    OUTPUT
+    ranked_movies - a dataframe with movies that are sorted by highest avg rating, more reviews,
+    then time, and must have more than 4 ratings
+    """
 
-        # Pull the average ratings and number of ratings for each movie
-        movie_ratings = reviews.groupby('movie_id')['rating']
-        avg_ratings = movie_ratings.mean()
-        num_ratings = movie_ratings.count()
-        last_rating = pd.DataFrame(reviews.groupby('movie_id').max()['date'])
-        last_rating.columns = ['last_rating']
+    # Pull the average ratings and number of ratings for each movie
+    movie_ratings = reviews.groupby('movie_id')['rating']
+    avg_ratings = movie_ratings.mean()
+    num_ratings = movie_ratings.count()
+    last_rating = pd.DataFrame(reviews.groupby('movie_id').max()['date'])
+    last_rating.columns = ['last_rating']
 
-        # Add Dates
-        rating_count_df = pd.DataFrame({'avg_rating': avg_ratings, 'num_ratings': num_ratings})
-        rating_count_df = rating_count_df.join(last_rating)
+    # Add Dates
+    rating_count_df = pd.DataFrame({'avg_rating': avg_ratings, 'num_ratings': num_ratings})
+    rating_count_df = rating_count_df.join(last_rating)
 
-        # merge with the movies dataset
-        movie_recs = movies.set_index('movie_id').join(rating_count_df)
+    # merge with the movies dataset
+    movie_recs = movies.set_index('movie_id').join(rating_count_df)
 
-        # sort by top avg rating and number of ratings
-        ranked_movies = movie_recs.sort_values(['avg_rating', 'num_ratings', 'last_rating'], ascending=False)
+    # sort by top avg rating and number of ratings
+    ranked_movies = movie_recs.sort_values(['avg_rating', 'num_ratings', 'last_rating'], ascending=False)
 
-        # for edge cases - subset the movie list to those with only 5 or more reviews
-        ranked_movies = ranked_movies[ranked_movies['num_ratings'] > 4]
+    # for edge cases - subset the movie list to those with only 5 or more reviews
+    ranked_movies = ranked_movies[ranked_movies['num_ratings'] > 4]
 
-        return ranked_movies
+    return ranked_movies
 
 
 def find_similar_movies(movie_id, movies_df):
-    '''
+    """
     INPUT
     movie_id - a movie_id
     movies_df - original movies dataframe
     OUTPUT
     similar_movies - an array of the most similar movies by title
-    '''
+    """
     # dot product to get similar movies
-    movie_content = np.array(movies_df.iloc[:,4:])
+    movie_content = np.array(movies_df.iloc[:, 4:])
     dot_prod_movies = movie_content.dot(np.transpose(movie_content))
 
     # find the row of each movie id
@@ -74,7 +76,7 @@ def find_similar_movies(movie_id, movies_df):
 
 
 def popular_recommendations(user_id, n_top, ranked_movies):
-    '''
+    """
     INPUT:
     user_id - the user_id (str) of the individual you are making recommendations for
     n_top - an integer of the number recommendations you want back
@@ -82,7 +84,7 @@ def popular_recommendations(user_id, n_top, ranked_movies):
 
     OUTPUT:
     top_movies - a list of the n_top recommended movies by movie title in order best to worst
-    '''
+    """
 
     top_movies = list(ranked_movies['movie'][:n_top])
 
--- recommender_template.py
+++ recommender_template.py
@@ -2,33 +2,47 @@ import numpy as np
 import pandas as pd
 import sys # can use sys to take command line arguments
 
-class Recommender():
-    '''
-    What is this class all about - write a really good doc string here
-    '''
+class Recommender:
+    """
+    Class to provide movie recommendations based on FunkSVD, content-based and knowledge based recommendation.
+    FunkSVD works with Matrix Factorization and uses latent features.
+    Content-based recommendation recommends the movies, that are similar to those the user rated high before.
+    Knowledge-based recommendation recommends the highest rated movies.
+    """
     def __init__(self, ):
-        '''
-        what do we need to start out our recommender system
-        '''
+        """
+        Initizalize Recommender
+        """
 
 
-
-    def fit(self, ):
-        '''
+    def fit(self, movies_path, reviews_path, latent_features = 15, learning_rate = 0.001, iters = 50):
+        """
         fit the recommender to your dataset and also have this save the results
         to pull from when you need to make predictions
-        '''
+        """
+
+        # Read in the data
+        movies = pd.read_csv(movies_path)
+        reviews = pd.read_csv(reviews_path)
+
+        # Create user-by-item matrix - nothing to do here
+        train_user_item = reviews[['user_id', 'movie_id', 'rating', 'timestamp']]
+        train_data_df = train_user_item.groupby(['user_id', 'movie_id'])['rating'].max().unstack()
+        train_data_np = np.array(train_data_df)
+
+
+
 
     def predict_rating(self, ):
-        '''
+        """
         makes predictions of a rating for a user on a movie-user combo
-        '''
+        """
 
     def make_recs(self,):
-        '''
+        """
         given a user id or a movie that an individual likes
         make recommendations
-        '''
+        """
 
 
 if __name__ == '__main__':
